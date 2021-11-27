044a341ffe57cf118d29c76304f0980604a7c246
Animesh Jaiswal <animeshgkp15@gmail.com>
2021-11-28 01:29:12 +0530

    URL

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 axios.defaults.withCredentials = true;
-
+const URL = process.env.BACKEND_URL || "http://localhost:4000"
 const axiosInstance =axios.create({
-    baseURL : "https://social-media-bkapi.herokuapp.com",
+    baseURL : URL,
 });
 
-export default axiosInstance;
+export default axiosInstance
