2cee1feb46070c6c62df9eeb631b2bb01f91fff7
Animesh Jaiswal <animeshgkp15@gmail.com>
2021-11-28 01:44:36 +0530

    Error resolove

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
 
-axios.defaults.withCredentials = true;
+
 const URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:4000"
 const axiosInstance =axios.create({
     baseURL : URL,
+    withCredentials:true
 });
 
+axiosInstance.interceptors.response.use(
+    response => (response), 
+    error => (Promise.reject(error.response.data.err))
+)
 export default axiosInstance
