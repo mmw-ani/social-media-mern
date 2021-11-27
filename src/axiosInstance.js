5ee2be302fe743e0d75db6ab3f3898e77f6d202d
Animesh Jaiswal <animeshgkp15@gmail.com>
2021-11-28 01:34:55 +0530

    Add env variables

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 axios.defaults.withCredentials = true;
-const URL = process.env.BACKEND_URL || "http://localhost:4000"
+const URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:4000"
 const axiosInstance =axios.create({
     baseURL : URL,
 });
